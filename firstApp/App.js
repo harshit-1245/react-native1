4cbbf92d554ce3fe5530751ed1b9d5e35588b5b7
harshit-506 <supriyamohapatra658@gmail.com>
2023-12-07 20:34:11 +0530

    first code in react native

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,ScrollView,TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text>
+        Some text
+      </Text>
+      <View>
+        <Text>Some other text inside the div</Text>
+        <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
+        style={{width:200,height:200}}
+        />
+      </View>
+      <TextInput
+      style={{
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+      }}
+      defaultValue='Write some text'
+      />
+      
+
+      
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
